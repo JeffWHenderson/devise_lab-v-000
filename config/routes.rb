a97ba2fa8f3e56a2a0351adb16236c103ec9dd91
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-30 05:54:06 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome/home'
+  root 'welcome#home'
   get '/about' => "welcome#about"
   #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
